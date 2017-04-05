ede11f8a934ded88ef39773c6978290a154f9370
PIGILAR\sx564 <sx5640@msn.com>
2017-04-05 16:08:14 -0400

    noodle2 to noodl2

--- noodl2-content/content/urls.py
+++ noodl2-content/content/urls.py
@@ -1,4 +1,4 @@
-"""noodle2 URL Configuration
+"""noodl2 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.10/topics/http/urls/
